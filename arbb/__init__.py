bb634863d72d302280a97de090679ef3eac7a163
Mustafa Aslan <mustafaslan63@gmail.com>
2024-01-14 01:13:32 +0300

    cubist_Added

--- arbb/__init__.py
+++ arbb/__init__.py
@@ -1,2 +1,2 @@
-from arbb.models import cat_forecaster, lightGBM_forecaster, xgboost_forecaster, AdaBoost_forecaster, RandomForest_forecaster
+from arbb.models import cat_forecaster, lightGBM_forecaster, xgboost_forecaster, AdaBoost_forecaster, RandomForest_forecaster, Cubist_forecaster
 from arbb.stats import unit_root_test, plot_PACF_ACF, rmse, fourier_terms
--- arbb/models.py
+++ arbb/models.py
@@ -576,4 +576,119 @@ class AdaBoost_forecaster:
 #         best_params = {i: int(best_hyperparams[i]) if i in ["n_estimators", "max_depth", "min_samples_split", "min_samples_leaf"] 
 #                            else best_hyperparams[i] for i in best_hyperparams}
         return space_eval(param_space, best_hyperparams)
+    
+class Cubist_forecaster:
+    def __init__(self, model, target_col, n_lag = None, lag_transform = None, cat_dict = None, drop_categ = None):
+        if (n_lag == None) and (lag_transform == None):
+            raise ValueError('Expected either n_lag or lag_transform args')
+        self.model = model
+        self.target_col = target_col
+        self.cat_var = cat_dict
+        self.n_lag = n_lag
+        self.lag_transform = lag_transform
+        self.drop_categ = drop_categ
+    
+        
+    def data_prep(self, df):
+        dfc = df.copy()
+        if self.cat_var is not None:
+            for col, cat in self.cat_var.items():
+                dfc[col] = dfc[col].astype('category')
+                dfc[col] = dfc[col].cat.set_categories(cat)
+            dfc = pd.get_dummies(dfc)
+        if self.drop_categ is not None:
+            for i in self.drop_categ:
+                dfc.drop(list(dfc.filter(regex=i)), axis=1, inplace=True)
+                
+        if self.target_col in dfc.columns:
+            if self.n_lag is not None:
+                for i in self.n_lag:
+                    dfc["lag"+"_"+str(i)] = dfc[self.target_col].shift(i)
+                
+            if self.lag_transform is not None:
+                df_array = np.array(dfc[self.target_col])
+                for i, j in self.lag_transform.items():
+                    dfc[i.__name__+"_"+str(j)] = i(df_array, j)    
+        dfc = dfc.dropna()
+        return dfc
+
+    def fit(self, df, param = None):
+        if param is not None:
+            model_cub =self.model(**param)
+        else:
+            model_cub =self.model()
+        model_df = self.data_prep(df)
+        self.X, self.y = model_df.drop(columns =self.target_col), model_df[self.target_col]
+        self.model_cub = model_cub.fit(self.X, self.y)
+
+    def forecast(self, n_ahead, x_test = None):
+        x_dummy = self.data_prep(x_test)
+
+        lags = self.y.tolist()
+        predictions = []
+        for i in range(n_ahead):
+            if x_test is not None:
+                x_var = x_dummy.iloc[i, 0:].tolist()
+            else:
+                x_var = []
+                
+            if self.n_lag is not None:
+                inp_lag = [lags[-l] for l in self.n_lag] # to get defined lagged variables 
+            else:
+                inp_lag = []
+            if self.lag_transform is not None:
+                lag_array = np.array(lags) # array is needed for transformation fuctions
+                transform_lag = []
+                for method, lag in self.lag_transform.items():
+                    tl = method(lag_array, lag)[-1]
+                    transform_lag.append(tl)
+            else:
+                transform_lag = []
+                    
+                    
+            inp = x_var+inp_lag+transform_lag
+            df_inp = pd.DataFrame(inp).T
+            df_inp.columns = self.X.columns
+
+            pred = self.model_cub.predict(df_inp)[0]
+            predictions.append(pred)
+            lags.append(pred)
+#             lags = lags[-max_lag:]
+        return np.array(predictions)
+
+    
+    def tune_model(self, df, cv_split, test_size, param_space, eval_metric, eval_num= 100):
+        tscv = TimeSeriesSplit(n_splits=cv_split, test_size=test_size)
+        
+        def objective(params):
+            model =self.model(**params)   
+                
+            metric = []
+            for train_index, test_index in tscv.split(df):
+                train, test = df.iloc[train_index], df.iloc[test_index]
+                x_test, y_test = test.iloc[:, 1:], np.array(test[self.target_col])
+                model_train = self.data_prep(train)
+                self.X, self.y = model_train.drop(columns =self.target_col), model_train[self.target_col]
+                self.model_cub = model.fit(self.X, self.y)
+                yhat = self.forecast(n_ahead =len(y_test), x_test=x_test)
+                if eval_metric.__name__== 'mean_squared_error':
+                    accuracy = eval_metric(y_test, yhat, squared=False)
+                else:
+                    accuracy = eval_metric(y_test, yhat)
+#                 print(str(accuracy)+" and len is "+str(len(test)))
+                metric.append(accuracy)
+            score = np.mean(metric)
+
+            print ("SCORE:", score)
+            return {'loss':score, 'status':STATUS_OK}
+            
+            
+        trials = Trials()
+
+        best_hyperparams = fmin(fn = objective,
+                        space = param_space,
+                        algo = tpe.suggest,
+                        max_evals = eval_num,
+                        trials = trials)
+        return space_eval(param_space, best_hyperparams)
             
